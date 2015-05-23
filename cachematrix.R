5688a00df6672d9240b4a92882a6c5c41dc5536e
Linda Harris <linda.harris@alum.mit.edu>
2015-05-23 14:42:03 -0700

    completed assignment 2 for R class by adopting code for caching mean of a vector to caching inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+## this pair of functions (makeCacheMatrix and cacheSolve) calculates the inverse of
+## a matrix, stores the result, and makes it accessible (without having to recalculate)
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list with functions to 
+## a) set (store) the matrix
+## b) get (return) the matrix
+## c) set (store/cache) the inverse of the matrix
+## d) get (return) the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     ## input parameters:
+     ##   x: a matrix for which you want to cache the inverse
+     ## output:
+     ##   a list of 
+     ##
+     ##   i represents the inverse of the input matrix
+     
+     i<-NULL
+     
+     set<-function(y) {
+               x<<-y
+               i<<-NULL  ## when item is added/modified, inverse is nulled out
+     }
+     get<-function() x  ##just return the input parameter
+     setinverse<-function(solve) i<<-solve  ## store the inverse
+     print (i)
+     getinverse<-function() i ## return the inverse
+     list(set=set, get=get, set=set, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve first checks to see whether the inverse of the input matrix has been stored
+## if so, it just returns that inverse
+## if not, it calculates and then stores the inverse for use next time
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     ## input parameters: 
+     ##   x: a list of functions, created by makeCacheMatrix
+     ## returns
+     ##   i: the inverse of the original matrix
+     
+     ## first time the function is called with a particular list, 
+     ## the inverse is calculated and stored/cached in the list.
+     ## ensuing times, the value of the inverse is just returned
+     
+     ## check if inverse has been stored. If so, retrieve it and return
+    i<-x$getinverse()
+    if(!is.null(i)) {
+          message("getting cached data")
+          return(i)
+    } 
+    
+    ## if inverse matrix not already stored, then
+    ## solve the inverse, cache it, and return the matrix.
+    data<-x$get()
+    i<-solve(data, ...)
+    x$setinverse(i)
+    i
 }
